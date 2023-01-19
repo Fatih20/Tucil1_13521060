30cd06d7bb3513509def94079f123eb234a40afb
Fatih20 <fnri39@protonmail.com>
2023-01-19 13:19:33 +0700

    Initial work on randomization.

---
+++ src/lib/solver/Solver.java
@@ -0,0 +1,31 @@
+package lib.solver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class Solver {
+    static private Set<List<String>> OperandRandomizer(List<String> givenCards) {
+        HashMap<String, Double> cardValues = new HashMap<String, Double>();
+        cardValues.put("2", 2.0);
+        cardValues.put("3", 3.0);
+        cardValues.put("4", 4.0);
+        cardValues.put("5", 5.0);
+        cardValues.put("6", 6.0);
+        cardValues.put("7", 7.0);
+        cardValues.put("8", 8.0);
+        cardValues.put("9", 9.0);
+        cardValues.put("10", 10.0);
+        cardValues.put("J", 11.0);
+        cardValues.put("Q", 12.0);
+        cardValues.put("K", 13.0);
+        cardValues.put("A", 14.0);
+
+        List<Double> translatedCards = new ArrayList<Double>();
+
+        for (int i = 0; i < 4; i++) {
+            translatedCards.add(cardValues.get(givenCards.get(i)));
+        }
+    }
+}
